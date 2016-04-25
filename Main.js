421029e29f2426c86722b088c2cba66103de9527
thegithubgeek <wfrankw9@gmail.com>
2016-04-25 12:31:15 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,5 +3,5 @@
 console.log("hia");
 //print();  
 //window.alert("Get Trolled");
-//window.open('http://thegithubgeek.github.io/','_blank')
+window.open('http://thegithubgeek.github.io/','_blank')
 //}
