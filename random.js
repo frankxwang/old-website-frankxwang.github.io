4cf334c958ad641312c671f288f266297d600349
thegithubgeek <wfrankw9@gmail.com>
2016-05-24 17:06:50 -0700

    Update random.js

--- random.js
+++ random.js
@@ -1,9 +1,8 @@
 var seed = 1009;
-var a = 19834931454567890;
-var b = 31754705969870959;
-var m = 13487650879804578;
+var a = 198349314545;
+var b = 31754705969;
+var m = 13487650879;
 setTimeout(function(){
   seed = (a*seed + b) % m;
   console.log(seed);
- },1000
-);
+ },2000);
