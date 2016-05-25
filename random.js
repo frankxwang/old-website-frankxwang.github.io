f61d8b5faa4db19a00e3fdc7bbc52b910e75695a
thegithubgeek <wfrankw9@gmail.com>
2016-05-24 17:19:33 -0700

    Update random.js

--- random.js
+++ random.js
@@ -2,7 +2,7 @@ var seed = 1009;
 var a = 198349314545;
 var b = 31754705969;
 var m = 13487650879;
-function() generate{
+function generate(){
   seed = (a*seed + b) % m;
   console.log(seed);
   console.log("Random");
